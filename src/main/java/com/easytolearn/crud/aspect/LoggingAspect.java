025b9f044b2b80b5a8d343cb5bf1d75d5930161c
Madhu <Madhu@DESKTOP-F14F966>
2021-03-07 11:08:11 +0530

    Added logging changes.

--- src/main/java/com/easytolearn/crud/aspect/LoggingAspect.java
+++ src/main/java/com/easytolearn/crud/aspect/LoggingAspect.java
@@ -1,8 +1,11 @@
 package com.easytolearn.crud.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.CodeSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,8 +17,30 @@ public class LoggingAspect {
 	public final static Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 	
 	@Before("execution(* com.easytolearn.crud.*.*(..))")
-	public void before(JoinPoint joinPoint){
-		log.info(" Request is"+joinPoint);	
+	public void controllerBefore(JoinPoint joinPoint){
+		log.info(" Request is "+getClassMetadata(joinPoint));	
+	}
+	
+	@AfterReturning("execution(* com.easytolearn.crud.*.*(..))")
+	public void controllerAfter(JoinPoint joinPoint){
+		log.info(" Response is "+getClassMetadata(joinPoint));	
+	}
+	
+	@Before("execution(* com.easytolearn.crud.service.*.*(..))")
+	public void serviceBefore(JoinPoint joinPoint){
+		log.info(" Service class "+getClassMetadata(joinPoint));	
+	}
+	
+	private String getClassMetadata(JoinPoint jp) {
+		CodeSignature signature = (CodeSignature) jp.getSignature();
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0 ; i < jp.getArgs().length ; i ++) {
+			builder.append(signature.getParameterNames()[i]);
+			builder.append(": ");
+			builder.append(jp.getArgs()[i].toString());
+			builder.append(" ");
+		}
+		return builder.toString();
 	}
 	
 }
